ae3341bce584addd94670cea8217edfbe0c46a93
Bradley White <brad@codebybrad.com>
2014-11-15 15:25:42 -0600

    Completed Assignment

--- Desktop/Bloc/Projects/Alcolator/Alcolator/AppDelegate.m
+++ Desktop/Bloc/Projects/Alcolator/Alcolator/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "BLCViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] ;
+    // Override point for customization after application launch.
+    BLCViewController *viewController = [[BLCViewController alloc] init];
+    self.window.rootViewController = viewController;
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
